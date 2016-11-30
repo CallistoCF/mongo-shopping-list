31667489daab823dec2bd84d7839bcb10d1f1be4
CallistoCF <chrisfrater@gmail.com>
2016-11-29 23:51:17 -0800

    travis + mbal + heroku

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                       (process.env.NODE_ENV === '172.31.0.0/16' ?
+                       (process.env.NODE_ENV === 'production' ?
                             'mongodb://localhost/shopping-list' :
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
